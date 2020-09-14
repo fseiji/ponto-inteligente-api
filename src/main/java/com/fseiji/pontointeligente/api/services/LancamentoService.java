fbfc6f8513d39f86114ef660c157397b280db28a
FERNANDO SEIJI MURAMATSU <Fernando.Seiji@riachuelo.com.br>
2020-09-14 17:16:14 -0300

    adicionando serviço/test de lançamento

--- src/main/java/com/fseiji/pontointeligente/api/services/LancamentoService.java
+++ src/main/java/com/fseiji/pontointeligente/api/services/LancamentoService.java
@@ -1,5 +1,39 @@
 package com.fseiji.pontointeligente.api.services;
 
-public interface LancamentoService {
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import com.fseiji.pontointeligente.api.entities.Lancamento;
+
+public interface LancamentoService {
+	
+	/**
+	 * Retorna uma lista paginada de lançamentos de um determinado funcionário
+	 * @param funcionarioId
+	 * @param pageRequest
+	 * @return Page<Lancamento>
+	 */
+	Page<Lancamento> buscarPorFuncionarioId(Long funcionarioId, PageRequest pageRequest);
+	
+	/**
+	 * Retorna um lançamento por ID
+	 * @param id
+	 * @return Optional<Lancamento>
+	 */
+	Optional<Lancamento> buscarPorId(Long id);
+
+	/**
+	 * Persiste um lançamento na base de dados
+	 * @param lancamento
+	 * @return lancamento
+	 */
+	Lancamento persiste(Lancamento lancamento);
+	
+	/**
+	 * Remove um lançamento da base de dados
+	 * @param id
+	 */
+	void remover(Long id);
 }
